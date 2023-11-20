24176ca4f1172904352e72ebabc82a87db6155d1
vmfkdla159 <vmfkdla159@gmail.com>
2023-11-20 12:40:55 +0900

    23-11-20

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const theLineWidth = document.querySelector("#line-width");
 const theLineColor = document.querySelector("#line-color");
+const fileInput = document.getElementById("file");
+const textInput = document.getElementById("text");
 const canvas = document.querySelector("canvas");
 const modeBtn = document.getElementById("mode-btn");
 const clearBtn = document.getElementById("clear-btn");
 const eraserBtn = document.getElementById("eraser-btn");
+const saveBtn = document.getElementById("save-btn");
+const fontOptions = document.getElementById("font-option");
+
 const colorArray = Array.from(document.getElementsByClassName("color-option"));
 const ctx = canvas.getContext("2d");
 
@@ -82,6 +87,37 @@ function onClickEraserBtn() {
   isFilling = false;
 }
 
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    file.value = null;
+  };
+}
+
+function onTextDoubleClick(event) {
+  const text = textInput.value;
+  if (text !== "") {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+}
+-0 +
+  function onSaveBtnClick() {
+    const url = canvas.toDataURL();
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "*.png";
+    a.click();
+  };
+
+canvas.addEventListener("dblclick", onTextDoubleClick);
+fileInput.addEventListener("change", onFileChange);
 canvas.addEventListener("mousemove", mouseMove);
 canvas.addEventListener("mousedown", startingPaint);
 canvas.addEventListener("mouseup", endingPaint);
@@ -92,9 +128,9 @@ modeBtn.addEventListener("click", onClickModeBtn);
 canvas.addEventListener("click", fillingMode);
 clearBtn.addEventListener("click", canvasClear);
 eraserBtn.addEventListener("click", onClickEraserBtn);
+saveBtn.addEventListener("click", onSaveBtnClick);
+fontOptions.addEventListener("change", onFontChange);
 
 colorArray.forEach((theLineColor) => {
   theLineColor.addEventListener("click", clickColor);
 });
-
-console.dir(canvas);
